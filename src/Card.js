f10017c5311d1decc768378f5a337ed353b5de08
Samarth Roy <samarthroy2106@gmail.com>
2021-04-29 15:08:25 +0530

    Added all player cards

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import './card.css';
 
 const Card = ({name, position, id, image}) => {
   return (
-    <div className='tc bg-yellow dib br3 pa3 ma2 grow bw2 shadow-5'>
+    <div className='tc bg-gold dib br3 pa3 ma2 grow bw2 shadow-5'>
       <img alt='player' src={image}/>
       <div>
         <h2>{name}</h2>
